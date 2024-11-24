f086b503fd8e2de187329a4cd36fd590aa11c414
gevsargsyaan <gevorg.sargsyaan01@gmail.com>
2024-11-24 22:52:29 +0400

    Change in .yml file

---
+++ build/third_party/googletest-main/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/root/Desktop/UDEMYCOURSE/projects/project1/third_party/googletest-main/googlemock
+# Build directory: C:/Users/root/Desktop/UDEMYCOURSE/projects/project1/build/third_party/googletest-main/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
---
+++ build/third_party/googletest-main/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/root/Desktop/UDEMYCOURSE/projects/project1/third_party/googletest-main/googletest
+# Build directory: C:/Users/root/Desktop/UDEMYCOURSE/projects/project1/build/third_party/googletest-main/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
